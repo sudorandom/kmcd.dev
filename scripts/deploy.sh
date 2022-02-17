09f7689d78f400973abe9cd283fbe6ceab69f800
Kevin McDonald <github@sudorandom.com>
2022-02-17 19:51:08 +0100

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git checkout --orphan gh-pages
+git --work-tree site add --all
+git --work-tree site commit -m 'gh-pages'
+git push origin HEAD:gh-pages --force
+git checkout -f master
