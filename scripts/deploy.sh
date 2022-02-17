e26db289b3c128fad0f70d5ec2e48c8d7e57930a
Kevin McDonald <github@sudorandom.com>
2022-02-17 20:08:20 +0100

    Push using git push action

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git checkout --orphan gh-pages
-git --work-tree site add --all
-git --work-tree site commit -m 'gh-pages'
-git push origin HEAD:gh-pages --force
-git checkout -f main
