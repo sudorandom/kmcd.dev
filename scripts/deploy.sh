369b40ea118fb3b7ff42704da54c0da8afcea789
Kevin McDonald <github@sudorandom.com>
2022-02-17 20:03:28 +0100

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ git checkout --orphan gh-pages
 git --work-tree site add --all
 git --work-tree site commit -m 'gh-pages'
 git push origin HEAD:gh-pages --force
-git checkout -f master
+git checkout -f main
