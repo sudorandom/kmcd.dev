0e09bb1ad298f75e06b18112a08768e1a9c4e800
Kevin McDonald <github@sudorandom.com>
2022-03-05 10:26:07 +0100

    Don't randomly gen ids for markdown images

--- markdown.js
+++ markdown.js
@@ -1,13 +1,8 @@
 
-function randId() {
-     return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
-}
-
 module.exports = exports = function renderer({ Marked, _relativeURL, _ID }) {
 
   Marked.image = ( href, title, text ) => {
-    let id = randId()
-    let out = `<a href="${ href }" class="has-popup-image" data-fancybox-group="markdown-images"><img src="${ href }" id="${ id }" class="center" alt="${ text }"`
+    let out = `<a href="${ href }" class="has-popup-image" data-fancybox-group="markdown-images"><img src="${ href }" class="center" alt="${ text }"`
 
     if( title ) {
       out += ` title="${ title }"`;
