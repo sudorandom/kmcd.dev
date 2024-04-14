6c1381aec7d0170a657a3b943a3af6ac4d405f09
Kevin McDonald <github@sudorandom.com>
2024-04-14 11:51:46 +0200

    Remove tags from mastodon pages (since they conflict with post tags)

--- import_mastodon.sh
+++ import_mastodon.sh
@@ -8,10 +8,9 @@ curl "https://infosec.exchange/api/v1/accounts/${ACCOUNT_ID}/statuses" \
 rm -rf ./content/updates/imported
 mkdir -p ./content/updates/imported
 
-jq -cr 'keys[] as $k | "\($k)\n\({date: .[$k].created_at, permalink: .[$k].url, params: .[$k]})"' data/mastodon.json | while read -r key; do
+jq -cr 'keys[] as $k | "\($k)\n\({title: "Mastodon Post", date: .[$k].created_at, permalink: .[$k].url, params: .[$k]})"' data/mastodon.json | while read -r key; do
   fname=$(jq --raw-output ".[$key].id" data/mastodon.json)
   read -r item
-  printf '%s\n' "$item" > "./content/updates/imported/$fname.html"
-
+  echo $item | jq 'del(.params.tags)' > "./content/updates/imported/$fname.html"
   echo $item | echo "$(jq --raw-output .params.content)" >> "./content/updates/imported/$fname.html"
 done
