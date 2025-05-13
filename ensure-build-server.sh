b3d1a5b8f138505c7d9790871e7201db77e13fed
Kevin McDonald <github@sudorandom.com>
2025-05-13 19:21:20 +0200

    chore: wait for build server to start

--- ensure-build-server.sh
+++ ensure-build-server.sh
@@ -4,3 +4,8 @@ set -e
 go install ./cmd/kmcd-render
 killall kmcd-render | true
 $(go env GOPATH)/bin/kmcd-render &
+until curl --output /dev/null --silent --head --fail http://127.0.0.1:7001; do
+    printf '.'
+    sleep 1
+done
+echo "build server started"
