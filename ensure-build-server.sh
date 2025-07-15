d8dc5984a8144ac1036b29002abe79b769fdbe50
Kevin McDonald <github@sudorandom.com>
2025-07-15 09:07:43 +0200

    just use go run

--- ensure-build-server.sh
+++ ensure-build-server.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-go install ./cmd/kmcd-render
 killall kmcd-render | true
-$(go env GOPATH)/bin/kmcd-render &
+go run ./cmd/kmcd-render &
 until curl --output /dev/null --silent --head --fail http://127.0.0.1:7001; do
     printf '.'
     sleep 1
